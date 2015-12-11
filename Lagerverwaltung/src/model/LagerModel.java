656a6bbb62086324659db605e510e4661fb8b26b
RedNik96 <nikidev96@yahoo.de>
2015-12-11 12:41:01 +0100

    neues TODO

--- Lagerverwaltung/src/model/LagerModel.java
+++ Lagerverwaltung/src/model/LagerModel.java
@@ -12,7 +12,7 @@ import core.exception.LagerUeberfuelltException;
  * Diese Klasse stellt die Daten für ein Lager bereit und verarbeitet diese
  */
 public class LagerModel extends Observable implements Serializable {
-	int maxKapazitaet, bestand;
+	int maxKapazitaet, bestand; //TODO verteilbare Menge implementieren
 	String name;
 	List<LagerModel> unterLager;
 	LagerModel oberLager;
